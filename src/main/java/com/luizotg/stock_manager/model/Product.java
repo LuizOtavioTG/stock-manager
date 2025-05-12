91b36c3d6a62aa4abc953b059acbb0a60dea6919
LuizOtavioTG <bc.luizo@gmail.com>
2025-05-12 08:08:29 -0300

    feat: defining the attribute structure in the Product entity

--- src/main/java/com/luizotg/stock_manager/model/Product.java
+++ src/main/java/com/luizotg/stock_manager/model/Product.java
@@ -1,3 +1,25 @@
 package com.luizotg.stock_manager.model;
 
-public class Product {}
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name="Product")
+@Table(name="products")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String brand;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private String unitOfMeasure;
+    private double costPrice;
+    private double salePrice;
+    private boolean active;
+}
